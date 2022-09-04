e7612d0a484939a2a10a8094063527b5ad9896ba
rickie <rickiemaweu@gmail.com>
2022-09-04 14:20:03 +0300

    commit tree 04c4a214d7b440e622743f4d91b299efd565e6ed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +15,39 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get "/reviews" do
+    reviews=Review.all
+    reviews.to_json
+  end
+
+  delete '/reviews/:id' do
+    review=Review.find(params[:id])
+    review.destroy
+    "Deleted successfully"
+    review.to_json
+  end
+
+  post "/reviews" do
+    review=Review.create(
+      score:params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+
+    review.to_json
+  end
+
+  patch "/reviews/:id" do 
+   review = Review.find(params[:id])
+   review.update(
+    score:params[:score],
+    comment:params[:comment]
+   )
+
+   review.to_json
   end
 
 end
